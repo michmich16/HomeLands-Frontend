236d727904d66fe489b1b025312e14009a2df57e
Michael P <102651473+michmich16@users.noreply.github.com>
2025-02-27 14:47:08 +0100

    mobile little

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,43 @@
 import { NavLink } from "react-router-dom";
 import s from './Navbar.module.scss';
-import { useState, useEffect } from "react";
-import { FaSearch } from "react-icons/fa";
-
+import { useState } from "react";
+import { FaSearch, FaBars, FaTimes } from "react-icons/fa";
 
 export const Navbar = () => {
+    const [searchWord, setSearchWord] = useState('');
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const [searchWord, setSearchWord] = useState('')
-    console.log('searchWord', searchWord)
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
         <>
             <nav className={s.navStyle}>
-                <ul>
+                <div className={s.navHeader}>
+                    <button className={s.burgerButton} onClick={toggleMenu}>
+                        {isMenuOpen ? <FaTimes /> : <FaBars />}
+                    </button>
+                </div>
+                <ul className={`${s.navLinks} ${isMenuOpen ? s.showMenu : ''}`}>
                     <li><NavLink to={'/'}>Forside</NavLink></li>
                     <li><NavLink to={'/estates'}>Boliger til salg</NavLink></li>
                     <li><NavLink to={'/login'}>Login</NavLink></li>
-                    <li><input type='search'
-                        variant='standard'
-                        name='search'
-                        id='search-field'
-                        value={searchWord}
-                        onChange={(event) => setSearchWord(event.target.value)} placeholder="Indtast søgeord" />
-                        <NavLink to={searchWord ? `/search/${searchWord}` : `/estates`}> <FaSearch /> </NavLink></li>
+                    <li>
+                        <input 
+                            type='search'
+                            name='search'
+                            id='search-field'
+                            value={searchWord}
+                            onChange={(event) => setSearchWord(event.target.value)} 
+                            placeholder="Indtast søgeord" 
+                        />
+                        <NavLink to={searchWord ? `/search/${searchWord}` : `/estates`}> 
+                            <FaSearch /> 
+                        </NavLink>
+                    </li>
                 </ul>
-
             </nav>
         </>
-    )
-}
+    );
+};
